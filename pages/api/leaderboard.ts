0917c56610b64a74e2d35353d0a51ef5f194eb79
wonderlandpark <wonderlandpark@outlook.kr>
2022-12-26 00:07:29 +0900

    style: linted

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -6,41 +6,41 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if(req.method === 'GET') {
+  if (req.method === 'GET') {
     const list = await prisma.leaderboard.findMany({
-      orderBy: [{ seconds: 'asc' }]
+      orderBy: [{ seconds: 'asc' }],
     })
 
     res.status(200).json(list)
-
-  } else if(req.method == 'POST') {
-    if(!authernate(req.headers.key as string)) return res.status(401).json({ error: 'Unauthorized' })
+  } else if (req.method == 'POST') {
+    if (!authernate(req.headers.key as string))
+      return res.status(401).json({ error: 'Unauthorized' })
     const { name, studentID, seconds, staff } = req.body
-    if(!name || !studentID || !seconds)
+    if (!name || !studentID || !seconds)
       return res.status(400).json({ error: 'Missing required fields' })
-  
+
     const list = await prisma.leaderboard.create({
       data: {
         name,
         studentID,
         seconds,
-        staff
-      }
+        staff,
+      },
     })
 
     return res.status(200).json(list)
-  } else if(req.method == 'DELETE') {
-    if(!authernate(req.headers.key as string)) return res.status(401).json({ error: 'Unauthorized' })
+  } else if (req.method == 'DELETE') {
+    if (!authernate(req.headers.key as string))
+      return res.status(401).json({ error: 'Unauthorized' })
     const { idx } = req.body
-    if(!idx) return res.status(400).json({ error: 'Missing required fields' })
+    if (!idx) return res.status(400).json({ error: 'Missing required fields' })
 
     const list = await prisma.leaderboard.delete({
       where: {
-        idx
-      }
+        idx,
+      },
     })
 
     return res.status(200).json(list)
   }
-  
 }
